1bfe1c1c91f3bf1dce2d5e8bc034095b6b135cd4
Saurabh <saurabhrjadhav26@gmail.com>
2024-09-29 14:24:02 +0530

    updated interface and interaction
    updated profile configuration and it's interaction

--- assets/scripts/eventManager.js
+++ assets/scripts/eventManager.js
@@ -1,15 +1,25 @@
 document.addEventListener('click', (event) => {
-  console.log(event.target);
-  switch (event.target.id) {
-    case 'main-profile' : {
-      let profileConfigClassList = $('#profile-config')[0].classList;
+  // console.log(event.target);
 
-      if (includes('active', profileConfigClassList)) {
-        profileConfigClassList.remove('active');
-      } else {
-        profileConfigClassList.add('active');
-      }
-    } break;
+  switch (event.target.id) {
+    case 'main-profile' : profileConfigLoadUnload();
+      break;
+    case 'profile-image' : profileConfigLoadUnload();
+      break;
     default : console.log('unlisted click event');
   }
 });
+
+const profileConfigLoadUnload = () => {
+  let
+    profileConfigClassList = $('#profile-config')[0].classList,
+    profileClassList = $('#main-profile')[0].classList;
+
+  if (includes('active', profileConfigClassList)) {
+    profileConfigClassList.remove('active');
+    profileClassList.remove('active');
+  } else {
+    profileConfigClassList.add('active');
+    profileClassList.add('active');
+  }
+}
