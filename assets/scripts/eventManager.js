72c42292ad6ead68f5e729a3b76b3a0f177b9d79
Saurabh <saurabhrjadhav26@gmail.com>
2024-10-03 12:59:33 +0530

    Added keyboard shortcut for scrolling workplace
    Shift + ArrowUp : scroll up
    Shift + ArrowDown : scroll down

--- assets/scripts/eventManager.js
+++ assets/scripts/eventManager.js
@@ -20,12 +20,24 @@ document.addEventListener('keydown', (event) => {
     } else if ((key === 'ArrowRight' && !includes('active', profile.classList)) || (key === 'ArrowLeft' && includes('active', profile.classList))) {
       profileConfigLoadUnload();
     }
+  } else if (keyLog['Shift']) {
+    if (!workplaceScrolling) {
+      if (key === 'ArrowUp') {
+        scrollWorkplace('up');
+      } else if (key === 'ArrowDown') {
+        scrollWorkplace('down');
+      }
+    }
   }
 });
 
-// set the key in `keyLog` to false if it's lifted
 document.addEventListener('keyup', (event) => {
-  keyLog[event.key] = false;
+  let key = event.key;
+  keyLog[key] = false;
+
+  if (key === 'Shift' || key === 'ArrowUp' || key === 'ArrowDown') {
+    workplaceScrolling = false;
+  }
 });
 
 // click event controls
@@ -45,60 +57,12 @@ document.addEventListener('mousedown', (event) => {
   let target = event.target;
 
   if (event.button === 0) {
-    // handle workplace scrolling
+    // trigger workplace scrolling
     if (!workplaceScrolling) {
-      if (target.id === 'workplace-scroller-up') {
-        workplaceScrolling = true;
-
-        let workplaceScrollerInterval = window.setInterval(() => {
-          if (!workplaceScrolling) {
-            clearInterval(workplaceScrollerInterval);
-          }
-
-          let
-            workplace = $('#graph-container-inner-box')[0],
-            workplaceTop = workplace.getClientRects()[0].top,
-            headerHeight = $('#main-header')[0].getClientRects()[0].height;
-
-          if (workplaceTop >= headerHeight) {
-            workplace.style.setProperty('top', `0`);
-            console.log('max scrolled');
-            workplaceScrolling = false;
-          } else {
-            workplace.style.setProperty('top', `${(workplaceTop - headerHeight) + workplaceScrollerSpeed}px`);
-          }
-
-          if (!workplaceScrolling) {
-            clearInterval(workplaceScrollerInterval);
-          }
-        }, workplaceScrollerFPS/1000);
-      } else if (target.id === 'workplace-scroller-down') {
-        workplaceScrolling = true;
-
-        let workplaceScrollerInterval = window.setInterval(() => {
-          if (!workplaceScrolling) {
-            clearInterval(workplaceScrollerInterval);
-          }
-
-          let
-            workplace = $('#graph-container-inner-box')[0],
-            workplaceHeight = workplace.getClientRects()[0].height,
-            workplaceTop = workplace.getClientRects()[0].top,
-            workplaceBottom = workplace.getClientRects()[0].bottom,
-            headerHeight = $('#main-header')[0].getClientRects()[0].height;
-
-          if (Math.round(workplaceBottom) <= window.visualViewport.height) {
-            workplace.style.setProperty('top', `${-(workplaceHeight - $('#main-content')[0].getClientRects()[0].height)}px`);
-            console.log('max scrolled');
-            workplaceScrolling = false;
-          } else {
-            workplace.style.setProperty('top', `${(workplaceTop - headerHeight) - workplaceScrollerSpeed}px`);
-          }
-
-          if (!workplaceScrolling) {
-            clearInterval(workplaceScrollerInterval);
-          }
-        }, workplaceScrollerFPS/1000);
+      if (target.id === 'workplace-scroller-down') {
+        scrollWorkplace('down');
+      } else if (target.id === 'workplace-scroller-up') {
+        scrollWorkplace('up');
       }
     }
   }
@@ -115,3 +79,60 @@ document.addEventListener('mouseup', (event) => {
 $('#file-upload-input').change(() => {
   $('.option.lable._1')[0].innerHTML = $('#file-upload-input')[0].files[0].name;
 });
+
+// handle scrolling up or down in the workplace
+const scrollWorkplace = (direction) => {
+  if (direction === 'up') {
+    workplaceScrolling = true;
+
+    let workplaceScrollerInterval = window.setInterval(() => {
+      if (!workplaceScrolling) {
+        clearInterval(workplaceScrollerInterval);
+      }
+
+      let
+        workplace = $('#graph-container-inner-box')[0],
+        workplaceTop = workplace.getClientRects()[0].top,
+        headerHeight = $('#main-header')[0].getClientRects()[0].height;
+
+      if (workplaceTop >= headerHeight) {
+        workplace.style.setProperty('top', `0`);
+        workplaceScrolling = false;
+      } else {
+        workplace.style.setProperty('top', `${(workplaceTop - headerHeight) + workplaceScrollerSpeed}px`);
+      }
+
+      if (!workplaceScrolling) {
+        clearInterval(workplaceScrollerInterval);
+      }
+    }, workplaceScrollerFPS/1000);
+  } else if (direction === 'down') {
+    workplaceScrolling = true;
+
+    let workplaceScrollerInterval = window.setInterval(() => {
+      if (!workplaceScrolling) {
+        clearInterval(workplaceScrollerInterval);
+      }
+
+      let
+        workplace = $('#graph-container-inner-box')[0],
+        workplaceHeight = workplace.getClientRects()[0].height,
+        workplaceTop = workplace.getClientRects()[0].top,
+        workplaceBottom = workplace.getClientRects()[0].bottom,
+        headerHeight = $('#main-header')[0].getClientRects()[0].height;
+
+      if (Math.round(workplaceBottom) <= window.visualViewport.height) {
+        workplace.style.setProperty('top', `${-(workplaceHeight - $('#main-content')[0].getClientRects()[0].height)}px`);
+        workplaceScrolling = false;
+      } else {
+        workplace.style.setProperty('top', `${(workplaceTop - headerHeight) - workplaceScrollerSpeed}px`);
+      }
+
+      if (!workplaceScrolling) {
+        clearInterval(workplaceScrollerInterval);
+      }
+    }, workplaceScrollerFPS/1000);
+  } else {
+    console.error(`Received invalid value for \`direciton\`.\nExpected: 'down' or 'up'\nReceived: '${direction}'`);
+  }
+}
